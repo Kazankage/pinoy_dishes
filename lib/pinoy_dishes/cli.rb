1a5830d7ad2ecbfc12b743c20fab941beb920278
Randel Jiao <randeljiao@gmail.com>
2019-10-05 22:48:11 +0000

    Adding code instructions for all

--- lib/pinoy_dishes/cli.rb
+++ lib/pinoy_dishes/cli.rb
@@ -13,7 +13,7 @@ puts "Do you have a hankering for Filipino Food? (Y/N)"
     choice = gets.strip
      case choice
       when "Y", "y", "Yes", "yes"
-        puts "Which dish would you like to learn how to prepare?"
+        puts "From the list below, which dish would you like to learn how to prepare?"
         when "N", "n", "No", "no"
           paalam
           exit
@@ -36,31 +36,93 @@ puts "Enter the name of the dish you wish to learn how to cook:"
     when "Adobo", "adobo", "Easy Chicken Adobo Recipe", 'easy chicken adobo recipe'
       puts "Here are the directions for Adobo: "
       @meals = PinoyDishes::Meals.to_cook
-      @meals.each_with_index do |meals, i|
+      @meals.each do |meals|
       
         puts "#{meals.instructions}"
       end
-=begin     puts "Here is the recipe for Adobo: https://panlasangpinoy.com/filipino-food-pork-adobo-recipe/"
+      # puts "Here is the recipe for Adobo: https://panlasangpinoy.com/filipino-food-pork-adobo-recipe/"
+    
     when "Sinigang", 'sinigang'
-      puts "Here is the recipe for Sinigang: https://panlasangpinoy.com/pork-sinigang-na-baboy-recipe/"
+       puts "Here are the directions for Sinigang: "
+      @meals = PinoyDishes::Meals.to_cook
+      @meals.each do |meals|
+      
+        puts "#{meals.instructions}"
+      end
+      # puts "Here is the recipe for Sinigang: https://panlasangpinoy.com/pork-sinigang-na-baboy-recipe/"
+    
     when "Nilnaga", "nilnaga", "Nilagang Litid ng Baka", "nilagang litid ng baka"
-      puts "Here is the recipe for Nilaga: https://panlasangpinoy.com/nilagang-litid-ng-baka/"
+       puts "Here are the directions for Niglaga: "
+      @meals = PinoyDishes::Meals.to_cook
+      @meals.each do |meals|
+      
+        puts "#{meals.instructions}"
+      end
+      # puts "Here is the recipe for Nilaga: https://panlasangpinoy.com/nilagang-litid-ng-baka/"
+    
     when "Bistek", "bistek", "Bistek Tagalog Recipe - Pinoy Beefsteak", "bistek tagalog recipe - pinoy beefsteak"
-      puts "Here is the recipe for Bistek: https://panlasangpinoy.com/bistek-tagalog-beefsteak-recipe/"
+       puts "Here are the directions for Bistek: "
+      @meals = PinoyDishes::Meals.to_cook
+      @meals.each do |meals|
+      
+        puts "#{meals.instructions}"
+      end
+      # puts "Here is the recipe for Bistek: https://panlasangpinoy.com/bistek-tagalog-beefsteak-recipe/"
+    
     when "Kare-Kare", "kare-kare", "Oxtail Kare-Kare Recipe", "oxtail kare-kare recipe"
-      puts "Here is the recipe for Kare-Kare: https://panlasangpinoy.com/oxtail-kare-kare-recipe-peanut-stew/"
+       puts "Here are the directions for Kare-Kare: "
+      @meals = PinoyDishes::Meals.to_cook
+      @meals.each do |meals|
+      
+        puts "#{meals.instructions}"
+      end
+      # puts "Here is the recipe for Kare-Kare: https://panlasangpinoy.com/oxtail-kare-kare-recipe-peanut-stew/"
+    
     when "Pinakbet", "pinakbet", "Pinakbet Tagalog", "pinakbet tagalog"
-      puts "Here is the recipe for Pinakbet: https://panlasangpinoy.com/pinakbet-tagalog-recipe-knr-sc/"
+       puts "Here are the directions for Pinakbet: "
+      @meals = PinoyDishes::Meals.to_cook
+      @meals.each do |meals|
+      
+        puts "#{meals.instructions}"
+      end
+      # puts "Here is the recipe for Pinakbet: https://panlasangpinoy.com/pinakbet-tagalog-recipe-knr-sc/"
+    
     when "Diniguan", "diniguan", "Dinuguan Recipe", "diniguan recipe"
-      puts "Here is the recipe for Diniguan: https://panlasangpinoy.com/dinuguan-blood-stew/"
+       puts "Here are the directions for Dinuguan: "
+      @meals = PinoyDishes::Meals.to_cook
+      @meals.each do |meals|
+      
+        puts "#{meals.instructions}"
+      end
+      # puts "Here is the recipe for Diniguan: https://panlasangpinoy.com/dinuguan-blood-stew/"
+    
     when "Bicol Express", "bicol express", "Pork Bicol Express Recipe", "pork bicol express recipe"
-      puts "Here is the recipe for Bicol Express: https://panlasangpinoy.com/pork-bicol-express-recipe/"
+       puts "Here are the directions for Bicol Express: "
+      @meals = PinoyDishes::Meals.to_cook
+      @meals.each do |meals|
+      
+        puts "#{meals.instructions}"
+      end
+      # puts "Here is the recipe for Bicol Express: https://panlasangpinoy.com/pork-bicol-express-recipe/"
+    
     when "Lumpia", "lumpia", "Chicken Lumpia", "chicken lumpia"
-      puts "Here is the recipe for Lumpia: https://panlasangpinoy.com/chicken-lumpia/"
+       puts "Here are the directions for Lumpia: "
+      @meals = PinoyDishes::Meals.to_cook
+      @meals.each do |meals|
+      
+        puts "#{meals.instructions}"
+      end
+      # puts "Here is the recipe for Lumpia: https://panlasangpinoy.com/chicken-lumpia/"
+    
     when "Tapsilog", "tapsilog", "Tapsilog Recipe", "tapsilog recipe"
-      puts "Here is the recipe for -silog: https://panlasangpinoy.com/tapsilog-recipe/"
-    end
-=end
+       puts "Here are the directions for Tapsilog: "
+      @meals = PinoyDishes::Meals.to_cook
+      @meals.each do |meals|
+      
+        puts "#{meals.instructions}"
+      end
+      # puts "Here is the recipe for -silog: https://panlasangpinoy.comhcipe/"
+    
   end
 
   def iba 
