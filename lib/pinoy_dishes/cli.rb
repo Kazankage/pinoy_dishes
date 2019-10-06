f5fc38467ed8031af9e2d2b39d89bb9600fc767f
Randel Jiao <randeljiao@gmail.com>
2019-10-06 00:21:42 +0000

    Fixing Exit

--- lib/pinoy_dishes/cli.rb
+++ lib/pinoy_dishes/cli.rb
@@ -7,6 +7,11 @@ class PinoyDishes::CLI
     paalam
   end
   
+  def paalam
+    puts "Ok. Sige. Salamat po!"
+    puts "Enjoy your meal!"
+  end
+  
   def gutom_na_tayo
 puts "Do you have a hankering for Filipino Food? (Y/N)"
     choice = nil
@@ -33,17 +38,16 @@ puts "Enter the name of the dish you wish to learn how to cook:"
     answer = nil
     answer = gets.strip
      case answer
-    when "Adobo", "adobo", "Easy Chicken Adobo Recipe", 'easy chicken adobo recipe'
+    when "Adobo", "adobo", "Easy Chicken Adobo Recipe", 'easy chicken adobo recipe', "1"
 
       puts "Here are the directions for Adobo: "
       puts PinoyDishes::Meals.scrape_adobo.instructions
-      puts PinoyDishes::Meals.scrape_adobo.ingredients
 =begin  @meals_adobo.each do |meals|
        puts "#{meals.instructions}"
       end
       puts "Here is the recipe for Adobo: https://panlasangpinoy.com/filipino-food-pork-adobo-recipe/"
 =end     
-    when "Sinigang", 'sinigang'
+    when "Sinigang", 'sinigang', "2"
  
        puts "Here are the directions for Sinigang: "
        puts PinoyDishes::Meals.scrape_sinigang.instructions
@@ -52,7 +56,7 @@ puts "Enter the name of the dish you wish to learn how to cook:"
       end
 =end      # puts "Here is the recipe for Sinigang: https://panlasangpinoy.com/pork-sinigang-na-baboy-recipe/"
     
-    when "Nilaga", "nilaga", "Nilagang Litid ng Baka", "nilagang litid ng baka"
+    when "Nilaga", "nilaga", "Nilagang Litid ng Baka", "nilagang litid ng baka", "3"
      
        puts "Here are the directions for Niglaga: "
        puts PinoyDishes::Meals.scrape_nilaga.instructions
@@ -61,7 +65,7 @@ puts "Enter the name of the dish you wish to learn how to cook:"
       end
 =end      # puts "Here is the recipe for Nilaga: https://panlasangpinoy.com/nilagang-litid-ng-baka/"
     
-    when "Bistek", "bistek", "Bistek Tagalog Recipe - Pinoy Beefsteak", "bistek tagalog recipe - pinoy beefsteak"
+    when "Bistek", "bistek", "Bistek Tagalog Recipe - Pinoy Beefsteak", "bistek tagalog recipe - pinoy beefsteak", "4"
      
        puts "Here are the directions for Bistek: "
        puts PinoyDishes::Meals.scrape_bistek.instructions
@@ -71,16 +75,16 @@ puts "Enter the name of the dish you wish to learn how to cook:"
       
       # puts "Here is the recipe for Bistek: https://panlasangpinoy.com/bistek-tagalog-beefsteak-recipe/"
 =end    
-    when "Kare-Kare", "kare-kare", "Oxtail Kare-Kare Recipe", "oxtail kare-kare recipe"
+    when "Kare-Kare", "kare-kare", "Oxtail Kare-Kare Recipe", "oxtail kare-kare recipe", "5"
        
        puts "Here are the directions for Kare-Kare: "
-      @meals = PinoyDishes::Meals.scrape_kare_kare.instructions
+       puts PinoyDishes::Meals.scrape_kare_kare.instructions
 =begin      @meals.each do |meals|
         puts "#{meals.instructions}"
       end
 =end      # puts "Here is the recipe for Kare-Kare: https://panlasangpinoy.com/oxtail-kare-kare-recipe-peanut-stew/"
     
-    when "Pinakbet", "pinakbet", "Pinakbet Tagalog", "pinakbet tagalog"
+    when "Pinakbet", "pinakbet", "Pinakbet Tagalog", "pinakbet tagalog", "6"
        
        puts "Here are the directions for Pinakbet: "
        puts PinoyDishes::Meals.scrape_pinakbet.instructions
@@ -89,7 +93,7 @@ puts "Enter the name of the dish you wish to learn how to cook:"
       end
 =end      # puts "Here is the recipe for Pinakbet: https://panlasangpinoy.com/pinakbet-tagalog-recipe-knr-sc/"
     
-    when "Diniguan", "diniguan", "Dinuguan Recipe", "diniguan recipe"
+    when "Diniguan", "diniguan", "Dinuguan Recipe", "diniguan recipe", "7"
 
        puts "Here are the directions for Dinuguan: "
        puts PinoyDishes::Meals.scrape_diniguan.instructions
@@ -98,7 +102,7 @@ puts "Enter the name of the dish you wish to learn how to cook:"
       end
 =end      # puts "Here is the recipe for Diniguan: https://panlasangpinoy.com/dinuguan-blood-stew/"
     
-    when "Bicol Express", "bicol express", "Pork Bicol Express Recipe", "pork bicol express recipe"
+    when "Bicol Express", "bicol express", "Pork Bicol Express Recipe", "pork bicol express recipe", "8"
    
        puts "Here are the directions for Bicol Express: "
        puts PinoyDishes::Meals.scrape_bicol_express.instructions
@@ -107,7 +111,7 @@ puts "Enter the name of the dish you wish to learn how to cook:"
       end
 =end      # puts "Here is the recipe for Bicol Express: https://panlasangpinoy.com/pork-bicol-express-recipe/"
     
-    when "Lumpia", "lumpia", "Chicken Lumpia", "chicken lumpia"
+    when "Lumpia", "lumpia", "Chicken Lumpia", "chicken lumpia", "9"
      
        puts "Here are the directions for Lumpia: "
        puts PinoyDishes::Meals.scrape_lumpia.instructions
@@ -116,7 +120,7 @@ puts "Enter the name of the dish you wish to learn how to cook:"
       end
 =end      # puts "Here is the recipe for Lumpia: https://panlasangpinoy.com/chicken-lumpia/"
     
-    when "Tapsilog", "tapsilog", "Tapsilog Recipe", "tapsilog recipe"
+    when "Tapsilog", "tapsilog", "Tapsilog Recipe", "tapsilog recipe", "10"
      
        puts "Here are the directions for Tapsilog: "
        puts PinoyDishes::Meals.scrape_tapsilog.instructions
@@ -124,7 +128,11 @@ puts "Enter the name of the dish you wish to learn how to cook:"
         puts "#{meals.instructions}"
       end
 =end      # puts "Here is the recipe for -silog: https://panlasangpinoy.comhcipe/"
-    
+    when "exit", "Exit", "EXIT"
+        paalam
+        exit
+    else
+      pano_gawin
   end
 
   def iba 
@@ -142,9 +150,6 @@ puts "Enter the name of the dish you wish to learn how to cook:"
   end
 end
   
-  def paalam
-    puts "Ok. Sige. Salamat po!"
-    puts "Enjoy your meal!"
-  end
+  
 end
 end
